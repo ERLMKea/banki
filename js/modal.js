065975b64ba977eb327dc1af881de3bca273f2e4
ERLMKea <erlm@kea.dk>
2022-11-17 09:03:37 +0100

    message section

--- js/modal.js
+++ js/modal.js
@@ -5,11 +5,25 @@ out("im in script")
 pbOpenModal = document.querySelector(".btn--show-modal")
 const modal = document.querySelector(".modal")
 const overlay = document.querySelector(".overlay")
+pbCloseModal = document.querySelector(".btn--close-modal")
 
 function openModal() {
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
 }
 
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 pbOpenModal.addEventListener('click', openModal)
+pbCloseModal.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
 
